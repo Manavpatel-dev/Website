1b2a6ab331ec6ac6e1c7af29d0fdca89cac1568f
Manavpatel-dev <81214448+Manavpatel-dev@users.noreply.github.com>
2021-03-23 12:14:19 +0530

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 03, 03, 10)}); // year, month, date, hour
+        });
+});		
+
